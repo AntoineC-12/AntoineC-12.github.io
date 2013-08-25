8a83b1bb8ceb9b353afb31c94cca3f5e0e2e0ce1
AntoineC-12 <pouillaude.a@gmail.com>
2013-08-26 00:32:24 +0200

    Revert 083351a..c1e57e6
    This rolls back to commit 083351af82912aa1535640ede0b7818a5ae12326.

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,7 +13,7 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(document).ready(function(){
-  $("section h2").each(function(){  //Petite modification pour éviter que les titres h1 n'apparaissent dans le menu et pour les h3 y apparaissent.
+  $("section h2, section h3").each(function(){  //Petite modification pour éviter que les titres h1 n'apparaissent dans le menu et pour les h3 y apparaissent.
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");

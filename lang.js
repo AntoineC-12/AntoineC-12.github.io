e7829877c861d235f42cc8219eb66af073b5682a
POUILLAUDE <lejoker-17@live.fr>
2013-08-28 12:43:56 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,17 @@
+<script src="path/to/jquery.min.js"></script>
+<script type="text/javascript" language="javascript">
+ 
+$(function() {
+    var language = 'italian';
+    $.ajax({
+        url: 'language.xml',
+        success: function(xml) {
+            $(xml).find('translation').each(function(){
+                var id = $(this).attr('id');
+                var text = $(this).find(language).text();
+                $("." + id).html(text);
+            });
+        }
+    });
+});
+</script>

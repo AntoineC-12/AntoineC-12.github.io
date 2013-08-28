794567d1d3cd6fd6848313f256f0753579b28e79
POUILLAUDE <lejoker-17@live.fr>
2013-08-28 14:30:19 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,4 +1,4 @@
-<script src="me/js/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" language="javascript">
  
 $(function() {

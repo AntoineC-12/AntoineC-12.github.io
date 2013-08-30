843d256b98cfee27e4794969b488dd2973d980ff
AntoineC-12 <lejoker-17@live.fr>
2013-08-30 19:39:53 +0200

    Revert 407d2dc..6b55f85
    This rolls back to commit 407d2dc3e4f25847e74eba688f96dcc0cf5e31fc.

--- lang.js
+++ lang.js
@@ -1,15 +1,15 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="path/to/jquery.min.js"></script>
 <script type="text/javascript" language="javascript">
  
 $(function() {
-    var language = 'fra';
+    var language = 'italian';
     $.ajax({
-        url: '../lang/lang.xml',
+        url: 'language.xml',
         success: function(xml) {
             $(xml).find('translation').each(function(){
                 var id = $(this).attr('id');
                 var text = $(this).find(language).text();
-                $("#" + id).html(text);
+                $("." + id).html(text);
             });
         }
     });

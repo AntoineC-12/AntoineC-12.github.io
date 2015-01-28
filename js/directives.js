eb263fd5cb2c439452f19ae2924920b07a39b2de
AntoineC-12 <lejoker-17@live.fr>
2015-01-28 14:25:19 +0100

    Test solution directive.

--- js/directives.js
+++ js/directives.js
@@ -44,7 +44,31 @@ myAppDirectives.directive('mySection', ['$translate', '$compile', function($tran
 	}
 ]);
 
-myAppDirectives.directive('myLinkedInCard', ['$window', function($window){
+myAppDirectives.directive('myLinkedInCard', ['$window', '$compile', function($window, $compile){
+	var linkedInCard='<script id="linkedIn" type="IN/MemberProfile" data-id="https://www.linkedin.com/pub/antoine-pouillaude/90/326/644" data-format="inline" data-related="false"></script>';
+	var linkedInlink='<script type="IN/MemberProfile" data-id="https://www.linkedin.com/pub/antoine-pouillaude/90/326/644" data-format="hover" data-text="Antoine POUILLAUDE" data-related="false"></script>';
+
+	var getTemplate = function(w) {
+		var width = w.width();
+		if (width > 1635)
+			return linkedInCard;
+		else
+			return linkedInlink;
+	};
+
+	var linkFN = function($scope, element, attrs) {
+		var w = angular.element($window);
+		$compile(getTemplate(w))(scope);
+	};
+
+	return {
+		restrict: 'E',
+		link: linkFN
+		}
+	};
+}]);
+
+myAppDirectives.directive('myLinkedIn', [function(){
 	return {
 		restrict: 'E',
 		template: '<script id="linkedIn" type="IN/MemberProfile" data-id="https://www.linkedin.com/pub/antoine-pouillaude/90/326/644" data-format="inline" data-related="false"></script>', 

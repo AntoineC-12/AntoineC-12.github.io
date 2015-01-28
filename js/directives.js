c22706da6059ec30a6edf61f5893a4714ec1016f
AntoineC-12 <lejoker-17@live.fr>
2015-01-28 14:08:58 +0100

    Update style and directive

--- js/directives.js
+++ js/directives.js
@@ -44,7 +44,7 @@ myAppDirectives.directive('mySection', ['$translate', '$compile', function($tran
 	}
 ]);
 
-myAppDirectives.directive('myLinkedInCard', ['$compile', function($compile){
+myAppDirectives.directive('myLinkedInCard', ['$window', function($window){
 	return {
 		restrict: 'E',
 		template: '<script id="linkedIn" type="IN/MemberProfile" data-id="https://www.linkedin.com/pub/antoine-pouillaude/90/326/644" data-format="inline" data-related="false"></script>', 

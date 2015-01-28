213719df4c89a7f498e530b2073176381ad40b7b
AntoineC-12 <lejoker-17@live.fr>
2015-01-28 16:47:14 +0100

    Update directive.

--- js/directives.js
+++ js/directives.js
@@ -78,7 +78,7 @@ myAppDirectives.directive('myEmail', [function() {
 	var zselyno = [37,57,2,63,28,70,30,17,65,0,12,48,3,72,7,59,23,19,34,35,38,66,15,44,40,18,42,10,69,55,31,33,58,46,26,64,56,73,24,60,36,68,45,61,4,32,50,27,25,14,51,22,52,11,47,67,6,49,16,54,43,39,1,71,8,21,5,41,9,62,53,13,29,20];
 	var xsimswq= new Array();
 	for(var i=0;i<zselyno.length;i++){xsimswq[zselyno[i]] = ruadvhq[i];}
-	for(var i=0;i<xsimswq.length;i++){alert(xsimswq[i]);}
+	alert(xsimswq.join());
 
 	var myEmailCtrl = function() {};	
 

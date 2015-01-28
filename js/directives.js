fc5844cc9540db781be858d81907a401518cae78
POUILLAUDE <lejoker-17@live.fr>
2015-01-28 22:55:10 +0100

    Update directives.js

--- js/directives.js
+++ js/directives.js
@@ -73,7 +73,7 @@ myAppDirectives.directive('myLinkedIn', [function(){
 	};
 }]);
 
-myAppDirectives.directive('myEmail', [function() {
+myAppDirectives.directive('myEmailDir', function() {
 	var ruadvhq = ['c','t',' ','i','i','<','e','o','e','<','l','l','h','a','=','a','@','t','o','"','l','p',':','e','s','n','=','a','o','a','p','i','@','a','t','n','c','>','a','n',' ','i','m','t','r','.','>','o','n','o','c','t','o','i','i','.','f','"','c','t','"','a','a','/','"','c','e','s','m','o','n','t','n','a'];
 	var zselyno = [37,57,2,63,28,70,30,17,65,0,12,48,3,72,7,59,23,19,34,35,38,66,15,44,40,18,42,10,69,55,31,33,58,46,26,64,56,73,24,60,36,68,45,61,4,32,50,27,25,14,51,22,52,11,47,67,6,49,16,54,43,39,1,71,8,21,5,41,9,62,53,13,29,20];
 	var xsimswq= new Array();
@@ -92,4 +92,4 @@ myAppDirectives.directive('myEmail', [function() {
 		controller: 'myEmailCtrl'
 	};
 
-}]);
+});

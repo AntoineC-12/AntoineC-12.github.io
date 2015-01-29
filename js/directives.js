875421e9acd7584a6143965c95aa9bf891e4ecfc
POUILLAUDE <lejoker-17@live.fr>
2015-01-29 11:17:03 +0100

    Update directives.js

--- js/directives.js
+++ js/directives.js
@@ -76,7 +76,7 @@ myAppDirectives.directive('myLinkedIn', [function(){
 myAppDirectives.directive('myEmailDir', ['$window', function($window) {
 	var ruadvhq = ['c','i','o','a','@','t','l','p',':','e','n','a','o','t','o','n','i','m','.','n','c','t','i','t','a','o'];
 	var qsDFGHS = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ,21 ,22 ,23 ,24 ,25 ];
-	var zselyno = [12,2,25,11,14,17,3,22,6,21,9,15,5,13,8,20,24,0,23,16,7,4,20,10,1,18];
+	var zselyno = [12,2,25,11,14,17,3,22,6,21,9,15,5,13,8,20,24,0,23,16,7,4,19,10,1,18];
         
 	var myEmailCtrl = function($scope) {
 		$scope.sendEmail = function() {
